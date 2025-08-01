1eb8c6584b42705686a5f3aac9618b65040b431b
Jose C Gonzalez <jcgonj1994@gmail.com>
2025-08-01 19:49:11 +0000

    feat: Genertor ok

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const randomNumbers = (arr) => {
+  return Math.floor(Math.random() * arr.length)
+}
+console.log(randomNumbers(who))
+
+const arrValue = (arr) => {
+  return arr[randomNumbers(arr)]
+}
+console.log(arrValue(who))
+
+const excuseGenerator = (arrOne, arrTwo, arrThree, arrFour) => {
+    return arrValue(who) + arrValue(action) + arrValue(what) + arrValue(when) 
+}
+console.log(excuseGenerator())
+
+const excuseDiv = document.getElementById('excuse')
+
+window.onload = function () {
   //write your code here
+  excuseDiv.innerHTML = excuseGenerator();
   console.log("Hello Rigo from the console!");
 };
+
+const btnGenrator = document.getElementById('generador');
+btnGenrator.addEventListener('click', () => {
+ excuseDiv.innerHTML = excuseGenerator();
+});
