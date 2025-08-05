a93aecf5070f824e14cf9bdab8a226822a784da5
Jose C Gonzalez <jcgonj1994@gmail.com>
2025-08-05 00:21:22 +0000

    feat: reduceExcusa ready

--- src/app.js
+++ src/app.js
@@ -14,27 +14,32 @@ const when = ['before the class', 'when I was sleeping', 'while I was exercising
 const randomNumbers = (arr) => {
   return Math.floor(Math.random() * arr.length)
 }
-console.log(randomNumbers(who))
 
 const arrValue = (arr) => {
   return arr[randomNumbers(arr)]
 }
-console.log(arrValue(who))
 
 const excuseGenerator = () => {
-    return arrValue(who) + arrValue(action) + arrValue(what) + arrValue(when) 
+  return arrValue(who) + arrValue(action) + arrValue(what) + arrValue(when)
 }
 console.log(excuseGenerator())
 
 const excuseDiv = document.getElementById('excuse')
+
+// no me quede conforme y quise hacer una excusa diferente con un metodo de los q aprendimos en clase //
+const sources = [who, action, what, when]
+const excuseWithReduce = () =>
+  sources.reduce((acc, arr) => acc + arrValue(arr), '');
+console.log("Reduce:", excuseWithReduce());
+//-------------------------------------------------------------------//
 
 window.onload = function () {
   //write your code here
   excuseDiv.innerHTML = excuseGenerator();
   console.log("Hello Rigo from the console!");
 };
-
+//aqui el botón extra para generar esa excusa
 const btnGenrator = document.getElementById('generador');
 btnGenrator.addEventListener('click', () => {
- excuseDiv.innerHTML = excuseGenerator();
+  excuseDiv.innerHTML = excuseGenerator();
 });
